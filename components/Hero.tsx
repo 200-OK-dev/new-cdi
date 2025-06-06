7cdfc7fca273e96dded9d1761563bff34d22cf05
200-OK-dev <200_ok_dev@proton.me>
2025-06-05 20:17:19 -0400

    5 junio 20:17

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,6 @@
 
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from "@/components/ui/button";
 
 export function Hero() {
   // Definir las palabras con sus respectivas clases de color
